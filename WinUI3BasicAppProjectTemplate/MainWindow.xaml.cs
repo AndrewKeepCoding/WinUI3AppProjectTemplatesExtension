5baabc8ca0c16e0e0b1c276cd1c65c4aa3162355
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2022-06-18 08:22:11 +0900

    Create WinUI3BasicAppProjectTemplate project

---
+++ WinUI3BasicAppProjectTemplate/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace WinUI3BasicAppProjectTemplate;
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
